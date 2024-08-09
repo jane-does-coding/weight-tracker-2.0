953d99bf605e3eca6eb4c64f3fb7d831624848d1
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-08-09 15:14:17 -0600

    start login

---
+++ app/components/Home.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const HomePage = () => {
+	return (
+		<div className="w-full min-h-screen items-center justify-center flex">
+			<div className="w-full relative flex flex-col gap-2">
+				<a
+					href="/"
+					className="w-[90vw] mx-auto bg-slate-700 hover:bg-slate-600 transition text-white flex items-center justify-center rounded-md py-1"
+				>
+					Login
+				</a>
+				<a
+					href="/"
+					className="w-[90vw] mx-auto bg-slate-700 hover:bg-slate-600 transition text-white flex items-center justify-center rounded-md py-1"
+				>
+					Register
+				</a>
+				<a
+					href="/"
+					className="w-[90vw] mx-auto bg-slate-700 hover:bg-slate-600 transition text-white flex items-center justify-center rounded-md py-1"
+				>
+					Enter
+				</a>
+				<a
+					href="/"
+					className="w-[90vw] mx-auto bg-slate-700 hover:bg-slate-600 transition text-white flex items-center justify-center rounded-md py-1"
+				>
+					Track
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
